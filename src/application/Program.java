5cdac6efa536345316074f8d8001283cffcadab8
Leonardo de Camargo <leonardo4215@live.com>
2024-08-21 12:25:15 -0400

    Updated prepareStatement method

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,10 +15,10 @@ public class Program {
         try {
             conn = DB.getConnection();
 
-            st = conn.prepareStatement(
-                            "DELETE FROM department "
-                            + "WHERE "
-                            + "Id = ?");
+            st = conn.prepareStatement("""
+                            DELETE FROM department
+                            WHERE Id = ?
+                            """);
 
             st.setInt(1, 2);
 
